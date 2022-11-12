4bc8d7e8655bcbdc02156e668a18abd7e561ba23
LasithEranga <lasitheranga1@gmail.com>
2022-11-12 15:40:52 +0530

    feat: user authorities

--- src/main/java/com/springsec/SpringsecApplication.java
+++ src/main/java/com/springsec/SpringsecApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.springsec.model.Role;
 import com.springsec.model.User;
@@ -35,6 +37,11 @@ public class SpringsecApplication {
 			userService.addRoleToUser("lasith", "ROLE_USER");
 			userService.addRoleToUser("lasith", "ROLE_ADMIN");
 		};
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
--- src/main/java/com/springsec/service/UserServiceImpl.java
+++ src/main/java/com/springsec/service/UserServiceImpl.java
@@ -6,16 +6,45 @@ import com.springsec.repo.RolesRepo;
 import com.springsec.repo.UserRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
-@Service @RequiredArgsConstructor @Transactional @Slf4j
-public class UserServiceImpl implements  UserService{
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class UserServiceImpl implements UserService, UserDetailsService {
 
     private final UserRepo userRepo;
     private final RolesRepo rolesRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // TODO Auto-generated method stub
+        User user = userRepo.findByUsername(username);
+        if (user == null) {
+            log.info("User not found");
+            throw new UsernameNotFoundException(username);
+
+        } else {
+            log.info("User found");
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        });
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), null);
+    }
 
     @Override
     public User saveUser(User user) {
@@ -50,4 +79,5 @@ public class UserServiceImpl implements  UserService{
         log.info("Fetaching all users");
         return userRepo.findAll();
     }
+
 }
