1cc6a1e63a10e62d53290e02c2cfa002f66239f6
LasithEranga <lasitheranga1@gmail.com>
2022-11-12 15:26:43 +0530

    feat: added user services and dummy users

--- src/main/java/com/springsec/SpringsecApplication.java
+++ src/main/java/com/springsec/SpringsecApplication.java
@@ -1,13 +1,40 @@
 package com.springsec;
 
+import java.util.ArrayList;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.springsec.model.Role;
+import com.springsec.model.User;
+import com.springsec.service.UserService;
 
 @SpringBootApplication
 public class SpringsecApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringsecApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(UserService userService) {
+		return args -> {
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_MANAGER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+			userService.saveRole(new Role(null, "ROLE_SUPERADMIN"));
+
+			userService.saveUser(new User(null, "Lasith", "lasith", "123456", new ArrayList<>()));
+			userService.saveUser(new User(null, "Eranda", "eranda", "123456", new ArrayList<>()));
+			userService.saveUser(new User(null, "Handapangoda", "handapangoda", "123456", new ArrayList<>()));
+
+			userService.addRoleToUser("lasith", "ROLE_SUPERADMIN");
+			userService.addRoleToUser("lasith", "ROLE_MANAGER");
+			userService.addRoleToUser("lasith", "ROLE_USER");
+			userService.addRoleToUser("lasith", "ROLE_ADMIN");
+		};
 	}
 
 }
--- src/main/java/com/springsec/controller/UserController.java
+++ src/main/java/com/springsec/controller/UserController.java
@@ -1,14 +1,21 @@
 package com.springsec.controller;
 
+import com.springsec.model.Role;
 import com.springsec.model.User;
 import com.springsec.service.UserService;
+
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -18,7 +25,34 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/users")
-    public ResponseEntity<List<User>>getUsers(){
+    public ResponseEntity<List<User>> getUsers() {
         return ResponseEntity.ok().body(userService.getUsers());
     }
+
+    @PostMapping("/user/save")
+    public ResponseEntity<User> saveUsers(@RequestBody User user) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+
+    @PostMapping("/role/save")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+
+    @PostMapping("/role/addtouser")
+    public ResponseEntity<?> addToUser(@RequestBody UserRole userRole) {
+        userService.addRoleToUser(userRole.getUsername(), userRole.getRolename());
+        return ResponseEntity.ok().build();
+    }
+
+}
+
+@Data
+class UserRole {
+    private String username;
+    private String rolename;
+
 }
